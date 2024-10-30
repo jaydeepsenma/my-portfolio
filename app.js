21e79aae36b1306187c2797ba107b7d5cb773d72
jaydeepsenma <rawatjay532@gmail.com>
2024-10-30 12:04:59 +0530

    Fix uploading onine server issue

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -9,6 +11,5 @@ app.get('/', (req, res) => {
   res.render('layout', { title: "Jaydeep Senma" });
 });
 
-app.listen(port, () => {
-  console.log(`Portfolio app listening at http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
